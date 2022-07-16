49e6cf88d5fa65fd9609233440fde9ddbee4cf2c
BarthelemyPavy <b.pavy@jellysmack.com>
2022-07-16 17:59:59 +0200

    🚧 add utils function for lightFM

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -13,6 +13,8 @@ import numpy as np
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+import seaborn as sns
+from lightfm.evaluation import precision_at_k, recall_at_k, auc_score
 from bs4 import BeautifulSoup
 from keybert import KeyBERT
 from sklearn.feature_extraction.text import CountVectorizer
@@ -393,3 +395,144 @@ def log_raise(
     if original_err:
         raise err from original_err
     raise err
+
+
+# From https://github.com/microsoft/recommenders/blob/main/recommenders/models/lightfm/lightfm_utils.py#L213
+def prepare_all_predictions(
+    data,
+    uid_map,
+    iid_map,
+    interactions,
+    model,
+    num_threads,
+    user_features=None,
+    item_features=None,
+):
+    """Function to prepare all predictions for evaluation.
+    Args:
+        data (pandas df): dataframe of all users, items and ratings as loaded
+        uid_map (dict): Keys to map internal user indices to external ids.
+        iid_map (dict): Keys to map internal item indices to external ids.
+        interactions (np.float32 coo_matrix): user-item interaction
+        model (LightFM instance): fitted LightFM model
+        num_threads (int): number of parallel computation threads
+        user_features (np.float32 csr_matrix): User weights over features
+        item_features (np.float32 csr_matrix):  Item weights over features
+    Returns:
+        pandas.DataFrame: all predictions
+    """
+    questions, users, preds = [], [], []  # noqa: F841
+    user = list(data.user_id.unique())
+    for question in data.question_id.unique():
+        question = [question] * len(user)
+        questions.extend(question)
+        users.extend(user)
+    all_predictions = pd.DataFrame(data={"question_id": questions, "user_id": users})
+    all_predictions["uid"] = all_predictions.question_id.map(uid_map)
+    all_predictions["iid"] = all_predictions.user_id.map(iid_map)
+
+    dok_weights = interactions.todok()
+    all_predictions["rating"] = all_predictions.apply(lambda x: dok_weights[x.uid, x.iid], axis=1)
+
+    all_predictions = all_predictions[all_predictions.rating < 1].reset_index(drop=True)
+    all_predictions = all_predictions.drop("rating", axis=1)
+
+    all_predictions["prediction"] = all_predictions.apply(
+        lambda x: model.predict(
+            user_ids=np.array([x["uid"]], dtype=np.int32),
+            item_ids=np.array([x["iid"]], dtype=np.int32),
+            user_features=user_features,
+            item_features=item_features,
+            num_threads=num_threads,
+        )[0],
+        axis=1,
+    )
+
+    return all_predictions[["question_id", "user_id", "prediction"]]
+
+
+def model_perf_plots(df):
+    """Function to plot model performance metrics.
+    Args:
+        df (pandas.DataFrame): Dataframe in tidy format, with ['epoch','level','value'] columns
+    Returns:
+        object: matplotlib axes
+    """
+    g = sns.FacetGrid(df, col="metric", hue="stage", col_wrap=2, sharey=False)
+    g = g.map(sns.scatterplot, "epoch", "value").add_legend()
+
+
+def track_model_metrics(
+    model, train_interactions, test_interactions, k=10, no_epochs=100, no_threads=8, show_plot=True, **kwargs
+):
+    """Function to record model's performance at each epoch, formats the performance into tidy format,
+    plots the performance and outputs the performance data.
+    Args:
+        model (LightFM instance): fitted LightFM model
+        train_interactions (scipy sparse COO matrix): train interactions set
+        test_interactions (scipy sparse COO matrix): test interaction set
+        k (int): number of recommendations, optional
+        no_epochs (int): Number of epochs to run, optional
+        no_threads (int): Number of parallel threads to use, optional
+        **kwargs: other keyword arguments to be passed down
+    Returns:
+        pandas.DataFrame, LightFM model, matplotlib axes:
+        - Performance traces of the fitted model
+        - Fitted model
+        - Side effect of the method
+    """
+    # initialising temp data storage
+    model_auc_train = [0] * no_epochs
+    model_auc_test = [0] * no_epochs
+
+    model_prec_train = [0] * no_epochs
+    model_prec_test = [0] * no_epochs
+
+    model_rec_train = [0] * no_epochs
+    model_rec_test = [0] * no_epochs
+
+    # fit model and store train/test metrics at each epoch
+    for epoch in range(no_epochs):
+        model.fit_partial(interactions=train_interactions, epochs=1, num_threads=no_threads, **kwargs)
+
+        model_auc_train[epoch] = auc_score(model, train_interactions, **kwargs).mean()
+        model_auc_test[epoch] = auc_score(
+            model, test_interactions, train_interactions=train_interactions, **kwargs
+        ).mean()
+
+        model_prec_train[epoch] = precision_at_k(model, train_interactions, k=k, **kwargs).mean()
+        model_prec_test[epoch] = precision_at_k(
+            model, test_interactions, train_interactions=train_interactions, k=k, **kwargs
+        ).mean()
+
+        model_rec_train[epoch] = recall_at_k(model, train_interactions, k=k, **kwargs).mean()
+        model_rec_test[epoch] = recall_at_k(
+            model, test_interactions, train_interactions=train_interactions, k=k, **kwargs
+        ).mean()
+
+    # collect the performance metrics into a dataframe
+    fitting_metrics = pd.DataFrame(
+        zip(model_prec_train, model_prec_test, model_rec_train, model_rec_test, model_auc_train, model_auc_test),
+        columns=[
+            "model_prec_train",
+            "model_prec_test",
+            "model_rec_train",
+            "model_rec_test",
+            "model_auc_train",
+            "model_auc_test",
+        ],
+    )
+    # convert into tidy format
+    fitting_metrics = fitting_metrics.stack().reset_index()
+    fitting_metrics.columns = ["epoch", "level", "value"]
+    # exact the labels for each observation
+    fitting_metrics["stage"] = fitting_metrics.level.str.split("_").str[-1]
+    fitting_metrics["metric"] = fitting_metrics.level.str.split("_").str[1]
+    fitting_metrics.drop(["level"], axis=1, inplace=True)
+    # replace the metric keys to improve visualisation
+    metric_keys = {"prec": "Precision", "rec": "Recall", "auc": "ROC AUC"}
+    fitting_metrics.metric.replace(metric_keys, inplace=True)
+    # plots the performance data
+    if show_plot:
+        model_perf_plots(fitting_metrics)
+    return fitting_metrics, model
